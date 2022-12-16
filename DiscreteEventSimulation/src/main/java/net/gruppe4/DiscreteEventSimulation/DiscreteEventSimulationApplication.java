c1e84e14ca5203a124b621441d61efc2335d5e19
Maxim Strzebkowski <maxim_st@gmx.de>
2022-12-16 12:33:55 +0100

    Initial Commit for actual Project

---
+++ DiscreteEventSimulation/src/main/java/net/gruppe4/DiscreteEventSimulation/DiscreteEventSimulationApplication.java
@@ -0,0 +1,13 @@
+package net.gruppe4.DiscreteEventSimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscreteEventSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscreteEventSimulationApplication.class, args);
+	}
+
+}
---
+++ DiscreteEventSimulation/src/test/java/net/gruppe4/DiscreteEventSimulation/DiscreteEventSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gruppe4.DiscreteEventSimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscreteEventSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
