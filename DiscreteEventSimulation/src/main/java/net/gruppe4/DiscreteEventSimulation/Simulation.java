eec4a6b6c690b16845f0cca128f52f541f0f2858
Hans Matziol <matziol@air-von-hans.speedport.ip>
2022-12-28 10:48:32 +0100

    move scripts to new Spring project

---
+++ DiscreteEventSimulation/src/main/java/net/gruppe4/DiscreteEventSimulation/Simulation.java
@@ -0,0 +1,15 @@
+package net.gruppe4.DiscreteEventSimulation;
+
+public class Simulation {
+
+	private final long id;
+
+	public Simulation(long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+}
