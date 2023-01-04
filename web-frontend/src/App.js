8dda3a5fa89145bb39b36f071c66e0fa6bc83290
maxim <m.strzebkowski@stud.uni-goettingen.de>
2023-01-04 11:53:52 +0100

    Added basic react router structure

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import {BrowserRouter,Routes,Route, Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is Version 0.1</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>This is the basic layout component <Outlet/></div>}>
+          <Route index element={<div>Welcome to the Homepage</div>} />
+          <Route path="upload" element={<div>The upload page component goes here</div>} />
+          <Route path="*" element={<div>That page does not exist</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
