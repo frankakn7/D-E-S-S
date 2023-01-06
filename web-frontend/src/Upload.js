a46f707180f309e80d4c5242d118a80e4037799d
Hans <hans.matziol@stud.uni-goettinge.de>
2023-01-06 15:16:15 +0100

    fixed Upload button bug

--- web-frontend/src/Upload.js
+++ web-frontend/src/Upload.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from "react-hook-form";
+import classes from "./Upload.module.css";
 
 
 const Upload = () => {
 
     const { register, handleSubmit } = useForm() 
+    const [selectedFile, setSelectedFile] = useState();
+
+    const onFileChange = (event) => {
+        setSelectedFile(event.currentTarget.files[0]);
+    };
 
     const onSubmit = (data) => {
       console.log(data)
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={classes.content}>
+            {/* <form onSubmit={handleSubmit(onSubmit)}>
                 <input ref={register} type="file" name="picture" />
                 <button>Submit</button>
-            </form>
+            </form> */}
+
+            <input type="file" onChange={onFileChange} />
+            {selectedFile && (<p>File Name: {selectedFile.name}</p>)}
         </div>
     )
 }
