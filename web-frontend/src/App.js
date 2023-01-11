332d5b7db2581fe28edeff3601c376d2e41f64b5
Maxim Strzebkowski <m.strzebkowski@stud.uni-goettingen.de>
2023-01-11 14:45:30 +0100

    formatting changes

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -10,51 +10,87 @@ import Results from "./pages/Results/Results";
 import Upload from "./pages/Upload/Upload";
 
 function App() {
-
-  const baseUrl = "http://localhost:8080"
+  const baseUrl = "http://localhost:8080";
 
   const { sendRequest: sendHttpRequest } = useHttp();
 
-  const [plans, setPlans] = useState([])
-  const [results, setResults] = useState([])
+  const [plans, setPlans] = useState([]);
+  const [results, setResults] = useState([]);
 
-  const { handlePlanUpload, handlePlanSimulate } = useApiPlanHandler(baseUrl, setPlans)
-  const { handleGetSimResult, handleGetSimStatus } = useApiSimCaseHandler(baseUrl, setResults)
+  const { handlePlanUpload, handlePlanSimulate } = useApiPlanHandler(
+    baseUrl,
+    setPlans
+  );
+  const { handleGetSimResult, handleGetSimStatus } = useApiSimCaseHandler(
+    baseUrl,
+    setResults
+  );
 
   useEffect(() => {
     const loadDataFromBackend = () => {
       const planPromise = sendHttpRequest({
-        url: baseUrl + "/api/plan/all"
-      })
+        url: baseUrl + "/api/plan/all",
+      });
 
       const resultPromise = sendHttpRequest({
-        url: baseUrl + "/api/sim/all"
-      })
+        url: baseUrl + "/api/sim/all",
+      });
 
-      Promise.all([planPromise, resultPromise]).then((values) => {
-        const [planData, resultData] = values;
+      Promise.all([planPromise, resultPromise])
+        .then((values) => {
+          const [planData, resultData] = values;
 
-        setPlans(planData.plans);
-        setResults(resultData.sim_cases);
-      }).catch((error) => console.log(error))
-    }
+          setPlans(planData.plans);
+          setResults(resultData.sim_cases);
+        })
+        .catch((error) => console.log(error));
+    };
 
     loadDataFromBackend();
-  }, [sendHttpRequest])
+  }, [sendHttpRequest]);
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Upload planUploadHandler={handlePlanUpload} planSimulateHandler={handlePlanSimulate} getSimCaseStatusHandler={handleGetSimStatus} getSimCaseResultHandler={handleGetSimResult}/>} />
-          <Route path="upload" element={<Upload planUploadHandler={handlePlanUpload} planSimulateHandler={handlePlanSimulate} getSimCaseStatusHandler={handleGetSimStatus} getSimCaseResultHandler={handleGetSimResult}/>} />
+          <Route
+            index
+            element={
+              <Upload
+                planUploadHandler={handlePlanUpload}
+                planSimulateHandler={handlePlanSimulate}
+                getSimCaseStatusHandler={handleGetSimStatus}
+                getSimCaseResultHandler={handleGetSimResult}
+              />
+            }
+          />
+          <Route
+            path="upload"
+            element={
+              <Upload
+                planUploadHandler={handlePlanUpload}
+                planSimulateHandler={handlePlanSimulate}
+                getSimCaseStatusHandler={handleGetSimStatus}
+                getSimCaseResultHandler={handleGetSimResult}
+              />
+            }
+          />
           <Route path="results/:id" element={<Results results={results} />} />
-          <Route path="plans" element={<Plans plans={plans} planSimulateHandler={handlePlanSimulate} getSimCaseStatusHandler={handleGetSimStatus} getSimCaseResultHandler={handleGetSimResult} />} />
+          <Route
+            path="plans"
+            element={
+              <Plans
+                plans={plans}
+                planSimulateHandler={handlePlanSimulate}
+                getSimCaseStatusHandler={handleGetSimStatus}
+                getSimCaseResultHandler={handleGetSimResult}
+              />
+            }
+          />
           <Route path="*" element={<FourOFour />} />
         </Route>
       </Routes>
     </BrowserRouter>
-
   );
 }
 
