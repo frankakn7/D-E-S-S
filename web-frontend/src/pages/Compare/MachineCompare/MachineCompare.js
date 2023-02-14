10f32a93575023d9e15011ee40de65ab795a5671
Maxim Strzebkowski <m.strzebkowski@stud.uni-goettingen.de>
2023-02-14 16:59:02 +0100

    Finished machine compare

--- web-frontend/src/pages/Compare/MachineCompare/MachineCompare.js
+++ web-frontend/src/pages/Compare/MachineCompare/MachineCompare.js
@@ -3,15 +3,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import Box from "../../../interface/Box/Box";
 import Button from "../../../interface/Button/Button";
+import CollapsableBox from "../../../interface/CollapsableBox/CollapsableBox";
 import classes from "./MachineCompare.module.css";
 import MachineCompareSelector from "./MachineCompareSelector/MachineCompareSelector";
+import MachineSummaryCompareHalf from "./MachineSummaryCompareHalf/MachineSummaryCompareHalf";
 
 const MachineCompare = (props) => {
     return (
         <div>
-            <Box titleText={<p>Machines Summary</p>}>
-                <p>Compare here</p>
-            </Box>
+            <CollapsableBox titleText="Machines Summary">
+              <div className={classes.collapsableContent}>
+                  <MachineSummaryCompareHalf allResults={props.resultsOne} className={classes.summaryHalf} />
+                  <MachineSummaryCompareHalf allResults={props.resultsTwo} className={classes.summaryHalf}/>
+              </div>
+            </CollapsableBox>
             <div className={classes.compareMachinesContainer}>
                 <div className={classes.half}>
                     <MachineCompareSelector
---
+++ web-frontend/src/pages/Compare/MachineCompare/MachineSummaryCompareHalf/MachineSummaryCompareHalf.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Box from "../../../../interface/Box/Box";
+import { MachineBreakdownIdleChart, MachineCostBarChart, MachineMakespanBarChart, MachineUtilisationBarChart } from "../../../Results/ResultsCharts/ResultsCharts";
+import classes from "./MachineSummaryCompareHalf.module.css";
+
+const MachineSummaryCompareHalf = (props) => {
+    return (
+        <Box titleText={<p>Simulation</p>} className={props.className}>
+            <div className={classes.boxContent}>
+                <MachineUtilisationBarChart
+                    className={classes.graphs}
+                    allResults={props.allResults}
+                />
+                <MachineMakespanBarChart
+                    className={classes.graphs}
+                    allResults={props.allResults}
+                />
+                <MachineBreakdownIdleChart
+                    className={classes.graphs}
+                    allResults={props.allResults}
+                />
+                <MachineCostBarChart
+                    className={classes.graphs}
+                    allResults={props.allResults}
+                />
+            </div>
+        </Box>
+    );
+};
+
+export default MachineSummaryCompareHalf;
